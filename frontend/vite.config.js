99bebd63c3aa19fe898938183073e9c0ddbb4185
twin-25 <tjssg678@gmail.com>
2026-06-09 21:40:18 -0400

    Update Docker configuration and dependencies for frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+});
